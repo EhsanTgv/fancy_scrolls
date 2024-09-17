d3de6e6a3b3d535954e35c9db8187380a3319641
EhsanTgv <EhsanTgv@gmail.com>
2024-09-17 09:51:33 +0300

    Implement Route generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fancy_scrolls/presentation/routes/generated_routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,26 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Home"),
-      ),
+      onGenerateRoute: RouteGenerator().generateRoute,
     );
   }
 }
