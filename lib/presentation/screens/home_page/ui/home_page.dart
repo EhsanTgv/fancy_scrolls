d7e8797a72603f5c14e59139be034c5e3d7c00f4
EhsanTgv <EhsanTgv@gmail.com>
2024-10-01 10:03:43 +0300

    Implement Fancy scroll

--- lib/presentation/screens/home_page/ui/home_page.dart
+++ lib/presentation/screens/home_page/ui/home_page.dart
@@ -10,6 +10,56 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            elevation: 0,
+            pinned: true,
+            centerTitle: false,
+            expandedHeight: 300,
+            stretch: true,
+            flexibleSpace: const FlexibleSpaceBar(
+              stretchModes: [
+                StretchMode.blurBackground,
+              ],
+              background: Image(
+                image: AssetImage("assets/images/flutter_background.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverAppBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            elevation: 0,
+            pinned: true,
+            bottom: const PreferredSize(
+              preferredSize: Size.fromHeight(-10),
+              child: SizedBox(),
+            ),
+            flexibleSpace: const SearchBar(),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20, bottom: 20, right: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
